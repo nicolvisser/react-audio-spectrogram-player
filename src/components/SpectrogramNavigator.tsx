6fea193bf15c4e5c45bef5fabe19046a0166548f
Nicol Visser <nicolvisser@yahoo.com>
2023-03-20 01:27:38 +0200

    adjust zoom window by dragging

--- src/components/SpectrogramNavigator.tsx
+++ src/components/SpectrogramNavigator.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { usePlayback } from "../context/PlaybackProvider";
 import { useZoom } from "../context/ZoomProvider";
 
@@ -8,25 +8,56 @@ interface SpectrogramNavigatorProps {
     height: number
 }
 
+const MINIMUM_ZOOM_WINDOW_DURATION = 0.01
+
 function SpectrogramNavigator(props: SpectrogramNavigatorProps) {
     const { children, width, height } = props
     const { duration } = usePlayback()
     const { startTime, endTime, setStartTime, setEndTime } = useZoom()
+    const svgRef = useRef<SVGSVGElement>(null);
+    const [dragStart, setDragStart] = useState<number | null>(null)
+    const [dragEnd, setDragEnd] = useState<number | null>(null)
 
-    useEffect(() => {
-        setStartTime(0.25)
-        setEndTime(0.75)
-    })
+    const getPointerCoordinate = (e: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
+        const boundingClientRect =
+            svgRef.current?.getBoundingClientRect();
+        if (boundingClientRect) {
+            const { left, right } = boundingClientRect
+            let newTime = duration * (e.clientX - left) / (right - left);
+            return newTime
+        }
+        return null
+    }
 
     const onPointerDown = (e: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
+        setDragStart(getPointerCoordinate(e))
+    }
 
+    const onPointerMove = (e: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
+        setDragEnd(getPointerCoordinate(e))
+    }
+
+    const onPointerUp = (e: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
+        if (dragStart && dragEnd) {
+            if (dragEnd - dragStart >= MINIMUM_ZOOM_WINDOW_DURATION) {
+                setStartTime(dragStart)
+                setEndTime(dragEnd)
+            }
+            else {
+                setStartTime(0)
+                setEndTime(duration)
+            }
+        }
+        setDragStart(null)
+        setDragEnd(null)
     }
 
     return (
-        <svg width={width} height={height} viewBox={`0,0,${duration},100`} preserveAspectRatio="none" onPointerDown={onPointerDown} >
+        <svg ref={svgRef} width={width} height={height} viewBox={`0,0,${duration},100`} preserveAspectRatio="none" onPointerDown={onPointerDown} onPointerUp={onPointerUp} onPointerMove={onPointerMove} >
             {children}
             <rect x={0} width={startTime} y="0" height="100" style={{ fill: 'white', opacity: 0.5 }} />
             <rect x={endTime} width={100 - endTime} y="0" height="100" style={{ fill: 'white', opacity: 0.5 }} />
+            {dragStart && dragEnd && dragEnd > dragStart && <rect x={dragStart} width={dragEnd - dragStart} y="0" height="100" style={{ fill: 'red', opacity: 0.5 }} />}
         </svg>
     )
 }
