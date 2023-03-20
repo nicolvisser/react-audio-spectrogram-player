f6a734eb589d32cf4a3e6703ff88124b35b191dd
Nicol Visser <nicolvisser@yahoo.com>
2023-03-21 00:31:30 +0200

    new examples from librispeech

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,28 @@
 import SpectrogramSurfer from "./components/SpectrogramSurfer";
 
-import sxx_long from "./assets/long.json";
-import sxx_backward from "./assets/backward.json";
-import sxx_bed from "./assets/bed.json";
-import sxx_bird from "./assets/bird.json";
-import sxx_cat from "./assets/cat.json";
-import sxx_dog from "./assets/dog.json";
-import sxx_down from "./assets/down.json";
-import sxx_eight from "./assets/eight.json";
+import sxx_0 from "./assets/19-198-0001.json";
+import sxx_1 from "./assets/26-495-0001.json";
+import sxx_2 from "./assets/27-123349-0001.json";
 
-import src_long from "./assets/long.wav";
-import src_backward from "./assets/backward.wav";
-import src_bed from "./assets/bed.wav";
-import src_bird from "./assets/bird.wav";
-import src_cat from "./assets/cat.wav";
-import src_dog from "./assets/dog.wav";
-import src_down from "./assets/down.wav";
-import src_eight from "./assets/eight.wav";
+import src_0 from "./assets/19-198-0001.wav";
+import src_1 from "./assets/26-495-0001.wav";
+import src_2 from "./assets/27-123349-0001.wav";
 
 const data = [
   {
-    key: "long",
-    sxx: sxx_long,
-    src: src_long
+    key: "0",
+    sxx: sxx_0,
+    src: src_0
   },
   {
-    key: "backward",
-    sxx: sxx_backward,
-    src: src_backward
+    key: "1",
+    sxx: sxx_1,
+    src: src_1
   },
   {
-    key: "bed",
-    sxx: sxx_bed,
-    src: src_bed
-  },
-  {
-    key: "bird",
-    sxx: sxx_bird,
-    src: src_bird
-  },
-  {
-    key: "cat",
-    sxx: sxx_cat,
-    src: src_cat
-  },
-  {
-    key: "dog",
-    sxx: sxx_dog,
-    src: src_dog
-  },
-  {
-    key: "down",
-    sxx: sxx_down,
-    src: src_down
-  },
-  {
-    key: "eight",
-    sxx: sxx_eight,
-    src: src_eight
+    key: "2",
+    sxx: sxx_2,
+    src: src_2
   }
 ]
 
