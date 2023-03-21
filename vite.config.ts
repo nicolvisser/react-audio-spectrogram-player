8b78bfe67c8905a80d2c09fa11e6a0dc0453c064
Nicol Visser <nicolvisser@yahoo.com>
2023-03-21 13:40:21 +0200

    configure dts in build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/lib/index.jsx'),
+      entry: path.resolve(__dirname, 'src/lib/index.tsx'),
       name: 'SpectrogramSurfer',
       fileName: (format) => `react-audio-spectrogram-player.${format}.js`
     },
@@ -18,5 +19,5 @@ export default defineConfig({
       }
     }
   },
-  plugins: [react()]
+  plugins: [react(), dts()]
 })
